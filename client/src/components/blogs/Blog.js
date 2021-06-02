33490dfe80d95705c0cac47fb3473bdea6d2a2d9
TheCoderAS <aaloksah766626@gmail.com>
2021-06-02 17:51:42 +0530

    Blog page completed

--- client/src/components/blogs/Blog.js
+++ client/src/components/blogs/Blog.js
@@ -1,18 +1,56 @@
 import React from 'react'
 import {blogsdata} from './blogsdata'
 
+const userdata={
+  _id:"1",
+  firstname:"Author",
+  lastname:"Name",
+  profilePic:"https://i.pinimg.com/originals/19/cf/78/19cf789a8e216dc898043489c16cec00.jpg",
+  about:"This is about the author. Lorem Ipsum has been the industry's standard dummy text when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+}
 const Blog=({match})=>{
-  //console.log(blogsdata[match.params.blog_id])
+  //console.log(match.params.blog_id)
+  var parameters=match.params.blog_id.split("+")
+  var category=parameters[0]
+  var id=parameters[1]
+  var data=blogsdata[category].filter((value)=>value._id===id)[0]
+  //console.log(data.image)
   return(
-    <div className="body blog">
-      <p>Blog Id: {match.params.blog_id}</p>
-      <p>To be completed after having database schema.</p>
-      <h1>{blogsdata[match.params.blog_id].title}</h1>
-      <h4>By: {blogsdata[match.params.blog_id].author}</h4>
-      <h4>{blogsdata[match.params.blog_id].caption}</h4>
-      <h6>Reviews</h6>
-      <h6>Comments</h6>
-      <h6>Likes</h6>
+    <div className="body" id="blog">
+      <div className="container">
+      <div className="row">
+          <div className="col-12">
+            <h1>{data.title}</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-10 text-center offset-1">
+            <img loading="lazy" className="img-fluid" src={data.image} alt={`${category}. Failed to load.`}/>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-10 text-center offset-1">
+            <p>{data.caption}</p>
+          </div>
+        </div>
+        <div className="row" id="author">
+          <div className="col-12">
+          <div className="card mb-3" key={userdata._id}>
+								<div className="row g-0">
+									<div className="col-md-4 text-center">
+										<img loading="lazy" width="auto" className="img-fluid" src={userdata.profilePic} alt="..."/>
+									</div>
+									<div className="col-md-8">
+										<div className="card-body">
+											<h5 className="card-title">{userdata.firstname} {userdata.lastname}</h5>
+											<p className="card-text"><small className="text-muted">{userdata.about}</small></p>
+										</div>
+									</div>
+								</div>
+							</div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/components/blogs/Blogs.js
+++ client/src/components/blogs/Blogs.js
@@ -4,6 +4,7 @@ import './Blogs.css'
 
 import {blogsdata} from './blogsdata'
 const Blogs=()=>{
+	//console.log(typeof(blogsdata),blogsdata)
 	const triggerTab=(tabName)=>{
 		var i,tabcontent,tablinks;
 		tabcontent=document.getElementsByClassName("tabcontent")
@@ -28,186 +29,210 @@ const Blogs=()=>{
 					<button className="tablink" id="Robotics-btn" onClick={()=>{triggerTab("Robotics")}}>Robotics</button>
 					<button className="tablink" id="Programming-btn" onClick={()=>{triggerTab("Programming")}}>Programming</button>
 					<button className="tablink" id="Exploration-btn" onClick={()=>{triggerTab("Exploration")}}>Exploration Series</button>
-					<button id="blog-btn" className="tablink" onClick={()=>{}}>New Blog</button>
+					{/* <button onClick={()=>window.location.href="/blogs/new"} id="blog-btn" className="tablink" onClick={()=>{}}>New Blog</button> */}
 				</div>
 
 				<div id="All" className="tabcontent container">
-				<div className="card mb-3">
-						<div className="row g-0">
-							<div className="col-md-4">
-								<img width="auto" className="img-fluid" src="https://i.guim.co.uk/img/media/e746109cf7315dbb58c23d0b903e4d9c579bfb25/0_0_4096_2459/master/4096.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=3a2a090066c5bfc15c0571dc7261968f" alt="..."/>
-							</div>
-							<div className="col-md-8">
-								<div className="card-body">
-									<h5 className="card-title">Title</h5>
-									<p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting</p>
-									<p className="card-text"><small className="text-muted">Last Updated 5 mins ago</small></p>
-									<div className="d-grid gap-2 d-md-flex justify-content-md-end">
-										<button className="read-btn me-md-2" type="button">Read More...</button>
-									</div>								
+				{
+						blogsdata.AI.map((value)=>{
+							//console.log(idx,value)
+							return(
+								<div className="card mb-3" key={value._id}>
+								<div className="row g-0">
+									<div className="col-md-4">
+										<img loading="lazy" width="auto" className="img-fluid" src={value.image} alt="..."/>
+									</div>
+									<div className="col-md-8">
+										<div className="card-body">
+											<h5 className="card-title">{value.title}</h5>
+											<p className="card-text">{value.caption}</p>
+											<p className="card-text"><small className="text-muted">{value.updatedAt}</small></p>
+											<div className="d-grid gap-2 d-md-flex justify-content-md-end">
+												<button onClick={()=>window.location.href=`/blogs/AI+${value._id}`} className="read-btn me-md-2" type="button">Read More...</button>
+											</div>								
+										</div>
+									</div>
 								</div>
 							</div>
-						</div>
-					</div>
-					<div className="card mb-3">
-						<div className="row g-0">
-							<div className="col-md-4">
-								<img width="auto" className="img-fluid" src="https://blogs.3ds.com/northamerica/wp-content/uploads/sites/4/2019/08/Robots-Square.jpg" alt="..."/>
+							)
+						})
+					}
+					{
+						blogsdata.Robotics.map((value)=>{
+							//console.log(idx,value)
+							return(
+								<div className="card mb-3" key={value._id}>
+								<div className="row g-0">
+									<div className="col-md-4">
+										<img loading="lazy" width="auto" className="img-fluid" src={value.image} alt="..."/>
+									</div>
+									<div className="col-md-8">
+										<div className="card-body">
+											<h5 className="card-title">{value.title}</h5>
+											<p className="card-text">{value.caption}</p>
+											<p className="card-text"><small className="text-muted">{value.updatedAt}</small></p>
+											<div className="d-grid gap-2 d-md-flex justify-content-md-end">
+												<button onClick={()=>window.location.href=`/blogs/Robotics+${value._id}`} className="read-btn me-md-2" type="button">Read More...</button>
+											</div>								
+										</div>
+									</div>
+								</div>
 							</div>
-							<div className="col-md-8">
-								<div className="card-body">
-									<h5 className="card-title">Title</h5>
-									<p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting</p>
-									<p className="card-text"><small className="text-muted">Last Updated 5 mins ago</small></p>
-									<div className="d-grid gap-2 d-md-flex justify-content-md-end">
-										<button className="read-btn me-md-2" type="button">Read More...</button>
-									</div>								</div>
+							)
+						})
+					}
+					{
+						blogsdata.Programming.map((value)=>{
+							//console.log(idx,value)
+							return(
+								<div className="card mb-3" key={value._id}>
+								<div className="row g-0">
+									<div className="col-md-4">
+										<img loading="lazy" width="auto" className="img-fluid" src={value.image} alt="..."/>
+									</div>
+									<div className="col-md-8">
+										<div className="card-body">
+											<h5 className="card-title">{value.title}</h5>
+											<p className="card-text">{value.caption}</p>
+											<p className="card-text"><small className="text-muted">{value.updatedAt}</small></p>
+											<div className="d-grid gap-2 d-md-flex justify-content-md-end">
+												<button onClick={()=>window.location.href=`/blogs/Programming+${value._id}`} className="read-btn me-md-2" type="button">Read More...</button>
+											</div>								
+										</div>
+									</div>
+								</div>
 							</div>
-						</div>
-					</div>
-					<div className="card mb-3">
-						<div className="row g-0">
-							<div className="col-md-4">
-								<img width="auto" className="img-fluid" src="https://thedisciplinedinvestor.com/blog/wp-content/uploads/2017/05/tdi_robo_banner-865x470.jpg" alt="..."/>
+							)
+						})
+					}
+					{
+						blogsdata.Exploration.map((value)=>{
+							//console.log(idx,value)
+							return(
+								<div className="card mb-3" key={value._id}>
+								<div className="row g-0">
+									<div className="col-md-4">
+										<img loading="lazy" width="auto" className="img-fluid" src={value.image} alt="..."/>
+									</div>
+									<div className="col-md-8">
+										<div className="card-body">
+											<h5 className="card-title">{value.title}</h5>
+											<p className="card-text">{value.caption}</p>
+											<p className="card-text"><small className="text-muted">{value.updatedAt}</small></p>
+											<div className="d-grid gap-2 d-md-flex justify-content-md-end">
+												<button onClick={()=>window.location.href=`/blogs/Exploration+${value._id}`} className="read-btn me-md-2" type="button">Read More...</button>
+											</div>								
+										</div>
+									</div>
+								</div>
 							</div>
-							<div className="col-md-8">
-								<div className="card-body">
-									<h5 className="card-title">Title</h5>
-									<p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting</p>
-									<p className="card-text"><small className="text-muted">Last Updated 5 mins ago</small></p>
-									<div className="d-grid gap-2 d-md-flex justify-content-md-end">
-										<button className="read-btn me-md-2" type="button">Read More...</button>
-									</div>								</div>
-							</div>
-						</div>
-					</div>
+							)
+						})
+					}
 				</div>
 				<div id="AI" className="tabcontent container">
-					<div className="card mb-3">
-						<div className="row g-0">
-							<div className="col-md-4">
-								<img width="auto" className="img-fluid" src="https://cdn.corporatefinanceinstitute.com/assets/artificial-intelligence-1024x512.jpeg" alt="..."/>
-							</div>
-							<div className="col-md-8">
-								<div className="card-body">
-									<h5 className="card-title">Title</h5>
-									<p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting</p>
-									<p className="card-text"><small className="text-muted">Last Updated 5 mins ago</small></p>
-									<div className="d-grid gap-2 d-md-flex justify-content-md-end">
-										<button className="read-btn me-md-2" type="button">Read More...</button>
-									</div>								
+					{
+						blogsdata.AI.map((value)=>{
+							//console.log(idx,value)
+							return(
+								<div className="card mb-3" key={value._id}>
+								<div className="row g-0">
+									<div className="col-md-4">
+										<img loading="lazy" width="auto" className="img-fluid" src={value.image} alt="..."/>
+									</div>
+									<div className="col-md-8">
+										<div className="card-body">
+											<h5 className="card-title">{value.title}</h5>
+											<p className="card-text">{value.caption}</p>
+											<p className="card-text"><small className="text-muted">{value.updatedAt}</small></p>
+											<div className="d-grid gap-2 d-md-flex justify-content-md-end">
+												<button onClick={()=>window.location.href=`/blogs/AI+${value._id}`} className="read-btn me-md-2" type="button">Read More...</button>
+											</div>								
+										</div>
+									</div>
 								</div>
 							</div>
-						</div>
-					</div>
-					<div className="card mb-3">
-						<div className="row g-0">
-							<div className="col-md-4">
-								<img width="auto" className="img-fluid" src="https://estio.training/wp-content/uploads/2020/02/ai.png" alt="..."/>
-							</div>
-							<div className="col-md-8">
-								<div className="card-body">
-									<h5 className="card-title">Title</h5>
-									<p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting</p>
-									<p className="card-text"><small className="text-muted">Last Updated 5 mins ago</small></p>
-									<div className="d-grid gap-2 d-md-flex justify-content-md-end">
-										<button className="read-btn me-md-2" type="button">Read More...</button>
-									</div>								
-								</div>
-							</div>
-						</div>
-					</div>
+							)
+						})
+					}
 				</div>
 				<div id="Robotics" className="tabcontent container">
-				<div className="card mb-3">
-						<div className="row g-0">
-							<div className="col-md-4">
-								<img width="auto" className="img-fluid" src="https://lh3.googleusercontent.com/proxy/PxNJ9necAKVWEGlxaYTinGV8c1mWQusMdI1nH3SUuzUA5_giIDjk7-A36elhXnJvHJp4CRLtNcuOs_ZSr5CDKyDZzlr_w4P36M45dIDZQB9ysihV11fnuHhf0Id9dGFQFryJZLs" alt="..."/>
-							</div>
-							<div className="col-md-8">
-								<div className="card-body">
-									<h5 className="card-title">Title</h5>
-									<p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting</p>
-									<p className="card-text"><small className="text-muted">Last Updated 5 mins ago</small></p>
-									<div className="d-grid gap-2 d-md-flex justify-content-md-end">
-										<button className="read-btn me-md-2" type="button">Read More...</button>
-									</div>								
+				{
+						blogsdata.Robotics.map((value)=>{
+							//console.log(idx,value)
+							return(
+								<div className="card mb-3" key={value._id}>
+								<div className="row g-0">
+									<div className="col-md-4">
+										<img loading="lazy" width="auto" className="img-fluid" src={value.image} alt="..."/>
+									</div>
+									<div className="col-md-8">
+										<div className="card-body">
+											<h5 className="card-title">{value.title}</h5>
+											<p className="card-text">{value.caption}</p>
+											<p className="card-text"><small className="text-muted">{value.updatedAt}</small></p>
+											<div className="d-grid gap-2 d-md-flex justify-content-md-end">
+												<button onClick={()=>window.location.href=`/blogs/Robotics+${value._id}`} className="read-btn me-md-2" type="button">Read More...</button>
+											</div>								
+										</div>
+									</div>
 								</div>
 							</div>
-						</div>
-					</div>
-					<div className="card mb-3">
-						<div className="row g-0">
-							<div className="col-md-4">
-								<img width="auto" className="img-fluid" src="https://d1qq9lwf5ow8iz.cloudfront.net/live-images-1/ImageDetail_85d5141d-e331-445b-8eb7-b49ba533e2bf_Large" alt="..."/>
-							</div>
-							<div className="col-md-8">
-								<div className="card-body">
-									<h5 className="card-title">Title</h5>
-									<p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting</p>
-									<p className="card-text"><small className="text-muted">Last Updated 5 mins ago</small></p>
-									<div className="d-grid gap-2 d-md-flex justify-content-md-end">
-										<button className="read-btn me-md-2" type="button">Read More...</button>
-									</div>								
-								</div>
-							</div>
-						</div>
-					</div>
+							)
+						})
+					}
 				</div>
 				<div id="Programming" className="tabcontent container">
-					<div className="card mb-3">
-						<div className="row g-0">
-							<div className="col-md-4">
-								<img width="auto" className="img-fluid" src="https://spectrum.ieee.org/image/MzI0NDQ4Ng.jpeg" alt="..."/>
-							</div>
-							<div className="col-md-8">
-								<div className="card-body">
-									<h5 className="card-title">Title</h5>
-									<p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting</p>
-									<p className="card-text"><small className="text-muted">Last Updated 5 mins ago</small></p>
-									<div className="d-grid gap-2 d-md-flex justify-content-md-end">
-										<button className="read-btn me-md-2" type="button">Read More...</button>
-									</div>								
+				{
+						blogsdata.Programming.map((value)=>{
+							//console.log(idx,value)
+							return(
+								<div className="card mb-3" key={value._id}>
+								<div className="row g-0">
+									<div className="col-md-4">
+										<img loading="lazy" width="auto" className="img-fluid" src={value.image} alt="..."/>
+									</div>
+									<div className="col-md-8">
+										<div className="card-body">
+											<h5 className="card-title">{value.title}</h5>
+											<p className="card-text">{value.caption}</p>
+											<p className="card-text"><small className="text-muted">{value.updatedAt}</small></p>
+											<div className="d-grid gap-2 d-md-flex justify-content-md-end">
+												<button onClick={()=>window.location.href=`/blogs/Programming+${value._id}`} className="read-btn me-md-2" type="button">Read More...</button>
+											</div>								
+										</div>
+									</div>
 								</div>
 							</div>
-						</div>
-					</div>
+							)
+						})
+					}
 				</div>
 				<div id="Exploration" className="tabcontent container">
-					<div className="card mb-3">
-						<div className="row g-0">
-							<div className="col-md-4">
-								<img width="auto" className="img-fluid" src="https://betanews.com/wp-content/uploads/2016/10/boy-explorer.jpg" alt="..."/>
-							</div>
-							<div className="col-md-8">
-								<div className="card-body">
-									<h5 className="card-title">Title</h5>
-									<p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting</p>
-									<p className="card-text"><small className="text-muted">Last Updated 5 mins ago</small></p>
-									<div className="d-grid gap-2 d-md-flex justify-content-md-end">
-										<button className="read-btn me-md-2" type="button">Read More...</button>
-									</div>								
+				{
+						blogsdata.Exploration.map((value)=>{
+							//console.log(idx,value)
+							return(
+								<div className="card mb-3" key={value._id}>
+								<div className="row g-0">
+									<div className="col-md-4">
+										<img loading="lazy" width="auto" className="img-fluid" src={value.image} alt="..."/>
+									</div>
+									<div className="col-md-8">
+										<div className="card-body">
+											<h5 className="card-title">{value.title}</h5>
+											<p className="card-text">{value.caption}</p>
+											<p className="card-text"><small className="text-muted">{value.updatedAt}</small></p>
+											<div className="d-grid gap-2 d-md-flex justify-content-md-end">
+												<button onClick={()=>window.location.href=`/blogs/Exploration+${value._id}`} className="read-btn me-md-2" type="button">Read More...</button>
+											</div>								
+										</div>
+									</div>
 								</div>
 							</div>
-						</div>
-					</div>
-					<div className="card mb-3">
-						<div className="row g-0">
-							<div className="col-md-4">
-								<img width="auto" className="img-fluid" src="https://swat.io/wp-content/uploads/2019/10/instagram-explore-1.jpg" alt="..."/>
-							</div>
-							<div className="col-md-8">
-								<div className="card-body">
-									<h5 className="card-title">Title</h5>
-									<p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting</p>
-									<p className="card-text"><small className="text-muted">Last Updated 5 mins ago</small></p>
-									<div className="d-grid gap-2 d-md-flex justify-content-md-end">
-										<button className="read-btn me-md-2" type="button">Read More...</button>
-									</div>								
-								</div>
-							</div>
-						</div>
-					</div>
+							)
+						})
+					}
 				</div>
 			</div>
 		</div>
