662b67d7ed959ba82a1681edb1a8470b667088c5
TheCoderAS <aaloksah766626@gmail.com>
2021-06-13 20:44:55 +0530

    User authentication comlete

---
+++ server/models/users.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const passportLocalMongoose=require('passport-local-mongoose')
+const Schema=mongoose.Schema;
+
+var  userSchema = new  Schema({
+    googleId:{
+        type:String,
+        default:''
+    },
+    firstname:{
+        type:String,
+        default:''
+    },
+    lastname:{
+        type:String,
+        default:''
+    },
+    admin:{
+        type:Boolean,
+        default:false
+    },
+    mentor:{
+      type:Boolean,
+      default:false
+    },
+    email:{
+        type:String,
+        default:''
+    },
+    phone:{
+        type:Number,
+        default:''
+    },
+    gender:{
+        type:String,
+        default:''
+    },
+    dob:{
+        type:String,
+        default:''
+    }
+});
+userSchema.plugin(passportLocalMongoose);
+module.exports=mongoose.model('User',userSchema)
